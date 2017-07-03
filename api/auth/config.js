c4058fb95b8e0226d04f13c47b857a72abf70c56
Maurício Costa <mauriciocosta.cm@gmail.com>
2017-07-03 17:47:38 -0300

    Autorização.

--- api/auth/config.js
+++ api/auth/config.js
@@ -2,6 +2,5 @@
 
 module.exports = Object.freeze({
   secret: process.env.TOKEN_SECRET,
-  tokenExpiration: process.env.TOKEN_EXPIRATION || 86400,
-  refreshTokenExpiration: process.env.REFRESH_TOKEN_EXPIRATION || 172800
+  tokenExpiration: process.env.TOKEN_EXPIRATION
 });
